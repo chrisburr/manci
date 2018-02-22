6324ab0d8ffefa319e7119433c08b23c58b8ddb6
Christopher Burr <christopher.burr@cern.ch>
2018-02-22 12:40:04 +0000

    Move to urban barnacle package

---
+++ urban_barnacle/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from . import utils
+# DIRAC must be initialised before importing other modules
+utils.initalise_dirac()  # NOQA
+from . import data_management
+from . import exceptions
+from . import XRootD
+
+
+__all__ = [
+    'data_management',
+    'exceptions',
+    'XRootD',
+]
